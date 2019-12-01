7797cc5b1c1b537a116952ab5ab7c05f7fdaf92b
Joris Valette <joris.valette@gmail.com>
2019-12-01 20:21:21 +0100

    aoc1 - 2

--- aoc1/src/main.rs
+++ aoc1/src/main.rs
@@ -14,20 +14,34 @@ fn main() -> Result<()> {
 fn part1(input: &str) -> Result<()> {
     let mut fuel = 0;
     for line in input.lines() {
-        let mass: u32 = line.parse()?;
+        let mass: i32 = line.parse()?;
         fuel += fuel_from_mass(mass);
     }
     writeln!(io::stdout(), "{}", fuel)?;
     Ok(())
 }
 
-fn part2(_input: &str) -> Result<()> {
-    // TODO
+fn part2(input: &str) -> Result<()> {
+    let mut fuel = 0;
+    for line in input.lines() {
+        let mass: i32 = line.parse()?;
+        fuel += fuel_from_mass_adjusted(mass);
+    }
+    writeln!(io::stdout(), "{}", fuel)?;
     Ok(())
 }
 
-fn fuel_from_mass(mass: u32) -> u32 {
+fn fuel_from_mass(mass: i32) -> i32 {
     (mass / 3) - 2
+}
+
+fn fuel_from_mass_adjusted(mass: i32) -> i32 {
+    let fuel = fuel_from_mass(mass);
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + fuel_from_mass_adjusted(fuel)
+    }
 }
 
 #[cfg(test)]
@@ -40,5 +54,12 @@ mod tests {
         assert_eq!(fuel_from_mass(14), 2);
         assert_eq!(fuel_from_mass(1969), 654);
         assert_eq!(fuel_from_mass(100756), 33583);
+    }
+
+    #[test]
+    fn test_fuel_adjusted() {
+        assert_eq!(fuel_from_mass_adjusted(14), 2);
+        assert_eq!(fuel_from_mass_adjusted(1969), 966);
+        assert_eq!(fuel_from_mass_adjusted(100756), 50346);
     }
 }
