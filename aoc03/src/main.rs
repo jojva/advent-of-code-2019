4b2c9617b08d6d7e23758868e0c58f0134f2c871
Joris Valette <joris.valette@algolia.com>
2019-12-04 19:01:05 +0100

    aoc3 - segment impl

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,7 +1,31 @@
+extern crate nalgebra as na;
+
+use na::{Point2, Vector2};
 use std::io::{self, Read, Write};
-use std::str::FromStr;
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+struct Segment {
+    p0: Point2<i32>,
+    p1: Point2<i32>,
+}
+
+impl Segment {
+    fn zero() -> Self {
+        Self {
+            p0: Point2::new(0, 0),
+            p1: Point2::new(0, 0),
+        }
+    }
+
+    fn intersects(rhs: &Segment) -> bool {
+
+    }
+
+    fn intersection_points(rhs: &Segment) -> Vec<Point2> {
+
+    }
+}
 
 fn main() -> Result<()> {
     let mut input = String::new();
@@ -13,48 +37,38 @@ fn main() -> Result<()> {
 }
 
 fn part1(input: &str) -> Result<()> {
-    let moves = parse_input(input);
-    writeln!(io::stdout(), "{:?}", moves)?;
-    Ok(())
-}
+    let moves_1 = parse_input(input.lines().nth(0).unwrap());
+    let moves_2 = parse_input(input.lines().nth(1).unwrap());
+    let mut segment_1 = Segment::zero();
+    for move_1 in &moves_1 {
+        let mut segment_2 = Segment::zero();
+        for move_2 in &moves_2 {
 
-fn parse_input(input: &str) -> Vec<Move> {
-    input
-        .trim()
-        .split(',')
-        .map(|m| m.parse().unwrap())
-        .collect()
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct Move {
-    x: i32,
-    y: i32,
-}
-
-impl FromStr for Move {
-    type Err = ();
-
-    fn from_str(s: &str) -> ::std::result::Result<Self, Self::Err> {
-        let magnitude: i32 = s[1..].parse().unwrap();
-        match s.chars().nth(0).unwrap() {
-            'U' => Ok(Move { x: 0, y: magnitude }),
-            'D' => Ok(Move {
-                x: 0,
-                y: -magnitude,
-            }),
-            'L' => Ok(Move {
-                x: -magnitude,
-                y: 0,
-            }),
-            'R' => Ok(Move { x: magnitude, y: 0 }),
-            _ => Err(()),
         }
     }
+    writeln!(io::stdout(), "{:?}", moves_1)?;
+    Ok(())
 }
 
 fn part2(_input: &str) -> Result<()> {
     Ok(())
+}
+
+fn parse_input(input: &str) -> Vec<Vector2<i32>> {
+    input
+        .trim()
+        .split(',')
+        .map(|movement| {
+            let magnitude: i32 = movement[1..].parse().unwrap();
+            match movement.chars().nth(0).unwrap() {
+                'U' => Vector2::new(0, magnitude),
+                'D' => Vector2::new(0, -magnitude),
+                'L' => Vector2::new(-magnitude, 0),
+                'R' => Vector2::new(magnitude, 0),
+                _ => panic!(),
+            }
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -65,7 +79,7 @@ mod tests {
     fn test_parse_input() {
         assert_eq!(
             parse_input("L54,R20"),
-            vec!(Move { x: -54, y: 0 }, Move { x: 20, y: 0 })
+            vec!(Vector2::new(-54, 0), Vector2::new(20, 0))
         );
     }
 }
